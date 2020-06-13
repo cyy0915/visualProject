51a270e85f30bf68d3a47515e53be185e37eec76
cyy0915 <33276595+cyy0915@users.noreply.github.com>
2020-06-13 21:54:25 +0800

    拟合预测的前端展示

--- interactWidget.js
+++ interactWidget.js
@@ -11,7 +11,16 @@ function createInteractWidget(data) {
     interactPara = {time:data.timeList[data.timeList.length-1], category: 'total_cases', countrySelect: [], recentSelect:'', continentSelect: ["AS","AF","EU"], countryFilter: [1000,1000]};
 
     d3.select('#countrySelect1').append('ul').selectAll('li')
-        .data(data.continentList).join('li').text(d=>data.continentName[d]).append('ul')
+        .data(data.continentList).join('li').text(d=>data.continentName[d])
+        .on('click', function (d) {
+            if (d3.select(this).select('ul').style('display')==='none') {
+                d3.select(this).select('ul').style('display', 'block')
+            }
+            else{
+                d3.select(this).select('ul').style('display', 'none')
+            }
+        })
+        .append('ul')
         .selectAll('li').data(d=>data.continentCountry[d]).join('li').text(d=>data.countryName[d]).on('click', click);
 
     d3.select('#categorySelect').selectAll('button')
@@ -55,6 +64,19 @@ function createInteractWidget(data) {
             filtByDeathCase(number);
         }
     })
+
+    $('#whole').click(function () {
+        if ($('#predict').attr('display')!=='none'){
+            $('#predict').hide();
+            $(this).hide().css('pointer-events', 'none');
+        }
+    })
+}
+
+function predictFromPython(middle, end) {
+    var tmpdate= new Date;
+    var picture = $.ajax({url:`predict/${interactPara.recentSelect}/${middle}/${end}`, async:false});
+    $('#predict img').attr('src', `plots/model_${interactPara.recentSelect}.png/${tmpdate.getTime()}`);
 }
 
 function updateBaseInfo(data) {
@@ -63,5 +85,23 @@ function updateBaseInfo(data) {
     <p>${globalData.countryName[data.iso_code]}</p>
     <p>确诊<br>${data.total_cases}</p>
     <p>新增<br>${data.new_cases}</p>
-    <p>死亡<br>${data.total_deaths}</p>`);
+    <p>死亡<br>${data.total_deaths}</p><p><button>拟合预测</button></p>`);
+    $('#basicInfo button').click(function () {
+        var countryData = globalData.countryData[interactPara.recentSelect];
+        var zeroCount = 0;
+        for (var i in countryData){
+            if (countryData[i].total_cases===0){
+                zeroCount+=1;
+            }
+        }
+        var timeMax = countryData.length-1-zeroCount;
+
+        predictFromPython(Math.floor(timeMax/2), timeMax);
+        $('#predict input').attr('max', timeMax)
+            .change(function () {
+                predictFromPython($(this).val(), timeMax);
+            }).val(Math.floor(timeMax/2));
+        $('#predict').show();
+        $('#whole').show().css('pointer-events', 'all');
+    })
 }
--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 import flask
 from flask import Flask
 import urllib.request as request
+import os
+import predict
+import pandas as pd
 
 app = Flask(__name__)
 dirpath = app.root_path
+
+@app.route('/predict/<country>/<middle>/<end>')
+def returnPredict(country, middle, end):
+    middle = int(middle)
+    end = int(end)
+    de = pd.read_csv("owid-covid-data.csv")
+    predict.plot_model(de, country, 0, 0,middle , middle+1, end)
+    return flask.send_from_directory(os.path.join(dirpath, 'plots'), "model_%s.png" % country)
+
+@app.route('/pic/<path>')
+def returnPic(path):
+    return flask.send_from_directory(os.path.join(dirpath, 'pic'), path)
+@app.route('/plots/<path>/<rand>')
+def returnPlot(path, rand):
+    return flask.send_from_directory(os.path.join(dirpath, 'plots'), path)
 @app.route('/<path>')
 def returnFile(path):
     if path!='owid-covid-data.csv':
